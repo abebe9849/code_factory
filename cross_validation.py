d7c2fef34b0c8432de50542db11861051219684b
Masatoshi Abe <55616956+abebe9849@users.noreply.github.com>
2020-12-31 01:40:22 +0900

    Create cross_validation.py

---
+++ cross_validation.py
@@ -0,0 +1,130 @@
+##stratify_groupk,multilabel_stratify_groupk
+import numpy as np
+from functools import partial
+from collections import Counter, defaultdict
+from tqdm import tqdm
+import pandas as pd
+
+
+def stratified_group_k_fold(X, y, groups, k, seed=None):
+    ''' https://www.kaggle.com/jakubwasikowski/stratified-group-k-fold-cross-validation '''
+    labels_num = np.max(y) + 1
+    y_counts_per_group = defaultdict(lambda: np.zeros(labels_num))
+    y_distr = Counter()
+    for label, g in zip(y, groups):
+        y_counts_per_group[g][label] += 1
+        y_distr[label] += 1
+
+    y_counts_per_fold = defaultdict(lambda: np.zeros(labels_num))
+    groups_per_fold = defaultdict(set)
+
+    def eval_y_counts_per_fold(y_counts, fold):
+        y_counts_per_fold[fold] += y_counts
+        std_per_label = []
+        for label in range(labels_num):
+            label_std = np.std([y_counts_per_fold[i][label] / y_distr[label] for i in range(k)])
+            std_per_label.append(label_std)
+        y_counts_per_fold[fold] -= y_counts
+        return np.mean(std_per_label)
+    
+    groups_and_y_counts = list(y_counts_per_group.items())
+    random.Random(seed).shuffle(groups_and_y_counts)
+
+    for g, y_counts in tqdm(sorted(groups_and_y_counts, key=lambda x: -np.std(x[1])), total=len(groups_and_y_counts)):
+        best_fold = None
+        min_eval = None
+        for i in range(k):
+            fold_eval = eval_y_counts_per_fold(y_counts, i)
+            if min_eval is None or fold_eval < min_eval:
+                min_eval = fold_eval
+                best_fold = i
+        y_counts_per_fold[best_fold] += y_counts
+        groups_per_fold[best_fold].add(g)
+
+    all_groups = set(groups)
+    for i in range(k):
+        train_groups = all_groups - groups_per_fold[i]
+        test_groups = groups_per_fold[i]
+
+        train_indices = [i for i, g in enumerate(groups) if g in train_groups]
+        test_indices = [i for i, g in enumerate(groups) if g in test_groups]
+
+        yield train_indices, test_indices
+        
+        
+        
+def multi_label_stratified_group_k_fold(label_arr: np.array, gid_arr: np.array, n_fold: int, seed: int=42):
+    """
+    create multi-label stratified group kfold indexs.
+
+    reference: https://www.kaggle.com/jakubwasikowski/stratified-group-k-fold-cross-validation
+    input:
+        label_arr: numpy.ndarray, shape = (n_train, n_class)
+            multi-label for each sample's index using multi-hot vectors
+        gid_arr: numpy.array, shape = (n_train,)
+            group id for each sample's index
+        n_fold: int. number of fold.
+        seed: random seed.
+    output:
+        yield indexs array list for each fold's train and validation.
+    """
+    np.random.seed(seed)
+    random.seed(seed)
+    start_time = time.time()
+    n_train, n_class = label_arr.shape
+    gid_unique = sorted(set(gid_arr))
+    n_group = len(gid_unique)
+
+    # # aid_arr: (n_train,), indicates alternative id for group id.
+    # # generally, group ids are not 0-index and continuous or not integer.
+    gid2aid = dict(zip(gid_unique, range(n_group)))
+#     aid2gid = dict(zip(range(n_group), gid_unique))
+    aid_arr = np.vectorize(lambda x: gid2aid[x])(gid_arr)
+
+    # # count labels by class
+    cnts_by_class = label_arr.sum(axis=0)  # (n_class, )
+
+    # # count labels by group id.
+    col, row = np.array(sorted(enumerate(aid_arr), key=lambda x: x[1])).T
+    cnts_by_group = coo_matrix(
+        (np.ones(len(label_arr)), (row, col))
+    ).dot(coo_matrix(label_arr)).toarray().astype(int)
+    del col
+    del row
+    cnts_by_fold = np.zeros((n_fold, n_class), int)
+
+    groups_by_fold = [[] for fid in range(n_fold)]
+    group_and_cnts = list(enumerate(cnts_by_group))  # pair of aid and cnt by group
+    np.random.shuffle(group_and_cnts)
+    print("finished preparation", time.time() - start_time)
+    for aid, cnt_by_g in sorted(group_and_cnts, key=lambda x: -np.std(x[1])):
+        best_fold = None
+        min_eval = None
+        for fid in range(n_fold):
+            # # eval assignment.
+            cnts_by_fold[fid] += cnt_by_g
+            fold_eval = (cnts_by_fold / cnts_by_class).std(axis=0).mean()
+            cnts_by_fold[fid] -= cnt_by_g
+
+            if min_eval is None or fold_eval < min_eval:
+                min_eval = fold_eval
+                best_fold = fid
+
+        cnts_by_fold[best_fold] += cnt_by_g
+        groups_by_fold[best_fold].append(aid)
+    print("finished assignment.", time.time() - start_time)
+
+    gc.collect()
+    idx_arr = np.arange(n_train)
+    for fid in range(n_fold):
+        val_groups = groups_by_fold[fid]
+
+        val_indexs_bool = np.isin(aid_arr, val_groups)
+        train_indexs = idx_arr[~val_indexs_bool]
+        val_indexs = idx_arr[val_indexs_bool]
+
+        print("[fold {}]".format(fid), end=" ")
+        print("n_group: (train, val) = ({}, {})".format(n_group - len(val_groups), len(val_groups)), end=" ")
+        print("n_sample: (train, val) = ({}, {})".format(len(train_indexs), len(val_indexs)))
+        
+        yield train_indexs, val_indexs

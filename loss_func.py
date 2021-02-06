98e9cb0cfde1db47fe814c2f15898bdd6a48c337
Masatoshi Abe <55616956+abebe9849@users.noreply.github.com>
2021-02-06 16:03:19 +0900

    Update loss_func.py
    2021/02/06 bersion

--- loss_func.py
+++ loss_func.py
@@ -5,26 +5,8 @@ import pandas as pd
 import torch.nn.functional as F
 from torch.nn import Parameter
 import math
+from torch.nn.modules.loss import _WeightedLoss
 
-class FocalLoss_BCE_withlogit(nn.Module):##マルチラベルのとき
-    def __init__(self, gamma=2.0, alpha=None, reduction="mean"):
-        super(FocalLoss_BCE_withlogit, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
-        self.reduction = reduction
-    def forward(self, input, target):
-        pt = torch.sigmoid(input)
-        error = torch.abs(pt - target)
-        log_error = torch.log(error)
-        loss = -1 * (1-error)**self.gamma * log_error
-        if self.reduction=="mean": 
-            return loss.mean()
-        elif self.reduction=="sum":
-            return loss.sum()
-        else:
-            return loss
 
 class Kloss(nn.Module):
     """
@@ -43,18 +25,40 @@ class Kloss(nn.Module):
         loss = 1.0 - (2.0*((input-self.y_shift)*(target-self.y_shift)).sum() - 1e-3)/\
         (((input-self.y_shift)**2).sum() + ((target-self.y_shift)**2).sum() + 1e-3)
         return loss
+class OneHotCrossEntropy(_WeightedLoss):
+
+    def __init__(self, weight=None, reduction='mean'):
+        super().__init__(weight=weight, reduction=reduction)
+        self.weight = weight
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        lsm = F.log_softmax(inputs, -1)
+
+        if self.weight is not None:
+            lsm = lsm * self.weight.unsqueeze(0)
+
+        loss = -(targets * lsm).sum(-1)
+
+        if  self.reduction == 'sum':
+            loss = loss.sum()
+        elif  self.reduction == 'mean':
+            loss = loss.mean()
+        return loss
 class SmoothCrossEntropy(nn.Module):
     
     # From https://www.kaggle.com/shonenkov/train-inference-gpu-baseline
-    def __init__(self, smoothing = 0.05):
+    def __init__(self, smoothing = 0.05,one_hotted=False):
         super().__init__()
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
+        self.one_hotted = one_hotted
 
     def forward(self, x, target):
         if self.training:
             x = x.float()
-            target = F.one_hot(target.long(), x.size(1))
+            if self.one_hotted!=True:
+                target = F.one_hot(target.long(), x.size(1))
             target = target.float()
             logprobs = F.log_softmax(x, dim = -1)
 
@@ -62,12 +66,14 @@ class SmoothCrossEntropy(nn.Module):
             nll_loss = nll_loss.sum(-1)
     
             smooth_loss = -logprobs.mean(dim=-1)
-
             loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-
             return loss.mean()
         else:
-            return F.cross_entropy(x, target.long())
+            if self.one_hotted!=True:
+                loss = F.cross_entropy(x, target.long())
+            else:
+                loss = OneHotCrossEntropy(x, target)
+            return loss
  
 
 
@@ -143,32 +149,106 @@ class AdaCos(nn.Module):
         return output
     
     
-## https://github.com/AlanChou/Truncated-Loss
-class TruncatedLoss(nn.Module):
 
-    def __init__(self, q=0.7, k=0.5, trainset_size=50000):
-        super(TruncatedLoss, self).__init__()
-        self.q = q
-        self.k = k
-        self.weight = torch.nn.Parameter(data=torch.ones(trainset_size, 1), requires_grad=False)
-             
-    def forward(self, logits, targets, indexes):
-        p = F.softmax(logits, dim=1)
-        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
+class MyCrossEntropyLoss(_WeightedLoss):
+    def __init__(self, weight=None, reduction='mean'):
+        super().__init__(weight=weight, reduction=reduction)
+        self.weight = weight
+        self.reduction = reduction
 
-        loss = ((1-(Yg**self.q))/self.q)*self.weight[indexes] - ((1-(self.k**self.q))/self.q)*self.weight[indexes]
-        loss = torch.mean(loss)
+    def forward(self, inputs, targets):
+        lsm = F.log_softmax(inputs, -1)
+
+        if self.weight is not None:
+            lsm = lsm * self.weight.unsqueeze(0)
+
+        loss = -(targets * lsm).sum(-1)
+
+        if  self.reduction == 'sum':
+            loss = loss.sum()
+        elif  self.reduction == 'mean':
+            loss = loss.mean()
+
         return loss
-    def update_weight(self, logits, targets, indexes):
-        p = F.softmax(logits, dim=1)
-        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
-        Lq = ((1-(Yg**self.q))/self.q)
-        Lqk = np.repeat(((1-(self.k**self.q))/self.q), targets.size(0))
-        Lqk = torch.from_numpy(Lqk).type(torch.cuda.FloatTensor)
-        Lqk = torch.unsqueeze(Lqk, 1)
-        
-        condition = torch.gt(Lqk, Lq)
-        self.weight[indexes] = condition.type(torch.cuda.FloatTensor)
+
+
+from torch.nn.parameter import Parameter
+from torch.autograd import Function
+class FocalLoss_ce(nn.Module):
+    def __init__(self, gamma=0, eps=1e-7):
+        super(FocalLoss_ce, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+    def forward(self, input, target):
+        logit = torch.nn.functional.softmax(input, dim=1)
+        logit = logit.clamp(self.eps, 1. - self.eps)
+        logit_ls = torch.log(logit)
+        loss = torch.nn.functional.nll_loss(logit_ls, target.long(), reduction="none")
+        view = target.size() + (1,)
+        index = target.view(*view)
+        loss = loss * (1 - logit.gather(1, index).squeeze(1)) ** self.gamma # focal loss
+        return loss.sum()
+
+class FocalLoss_BCE_withlogit(nn.Module):
+    #
+    def __init__(self, alpha=1, gamma=2, logits=True, reduction=False):
+        super(FocalLoss_BCE_withlogit, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduction = reduction
+    #    
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs.float(), targets.float(), reduction='none')
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs.float(), targets.float(), reduction='none')
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+        if self.reduction:
+            return torch.mean(F_loss)
+        else:
+            return torch.sum(F_loss)
+
+class FocalLoss_CE(nn.Module):
+    #
+    def __init__(self, alpha=1, gamma=2, reduction=False):
+        super(FocalLoss_CE, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+    #    
+    def forward(self, inputs, targets):
+        CE_loss = F.cross_entropy(inputs, targets.long(),reduction='none')
+        pt = torch.exp(-CE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * CE_loss
+        if self.reduction:
+            return torch.mean(F_loss)
+        else:
+            return torch.sum(F_loss)
+
+class FocalCosineLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, xent=.1):
+        super(FocalCosineLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+
+        self.xent = xent
+
+        self.y = torch.Tensor([1]).cuda()
+
+    def forward(self, input, target, reduction="mean"):
+        cosine_loss = F.cosine_embedding_loss(input, F.one_hot(target.long(), num_classes=input.size(-1)), self.y, reduction=reduction)
+
+        cent_loss = F.cross_entropy(F.normalize(input), target.long(), reduce=False)
+        pt = torch.exp(-cent_loss)
+        focal_loss = self.alpha * (1-pt)**self.gamma * cent_loss
+
+        if reduction == "mean":
+            focal_loss = torch.mean(focal_loss)
+
+        return cosine_loss + self.xent * focal_loss
+
     
 
 
